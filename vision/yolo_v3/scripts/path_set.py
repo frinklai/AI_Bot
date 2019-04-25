d9ccce16248d72bc898bd7bf9ec25583507d3d24
frinklai <frinklai@gmail.com>
2019-04-25 15:10:18 +0800

    add path for train yolo3

--- vision/yolo_v3/scripts/path_set.py
+++ vision/yolo_v3/scripts/path_set.py
@@ -3,4 +3,9 @@ model_data_path         = "/home/iclab-arm/AI_Bot_ws/src/AI_Bot/vision/yolo_v3/"
 font_path               = "/home/iclab-arm/AI_Bot_ws/src/AI_Bot/vision/yolo_v3/"
 python3_site_pkg_path   = "/home/iclab-arm/.local/lib/python3.5/site-packages/"
 
-
+# below are for train.py
+annotation_path = '../Train_labels/soda.txt'
+log_dir         = '../weights/soda/'
+classes_path    = '../model_data/task_class.txt'
+anchors_path    = '../model_data/yolo_anchors.txt'
+weights_path    = '../model_data/yolo_weights.h5'
