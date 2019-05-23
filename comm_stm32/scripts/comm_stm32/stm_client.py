9bacedd7d3341e7e7888cae6a74d4dcc2afedf53
frinklai <frinklai@gmail.com>
2019-05-23 14:48:42 +0800

    add new control mode for longer delay

--- comm_stm32/scripts/comm_stm32/stm_client.py
+++ comm_stm32/scripts/comm_stm32/stm_client.py
@@ -15,10 +15,13 @@ class Gripper:
         num = self.Conver_cmd_to_id(cmd_id)
         try:
             control_gripper = rospy.ServiceProxy('gripper_service', gripper_cmd)    
-            resp1 = control_gripper(int(num))          
+            resp1 = control_gripper(int(num))         
             if(cmd_id != 'Stop'):
                 rospy.loginfo("[client] Send command success, wait for gripper do task" ); 
-                rospy.sleep(self.TASK_EXE_TIME)                      # wait for gripper complete task
+                if(cmd_id=='Catch_all2'):
+                    rospy.sleep(5)                      # wait for gripper complete task
+                else:
+                    rospy.sleep(self.TASK_EXE_TIME)                      # wait for gripper complete task
                 self.Send_Gripper_Command('Stop')        # focus stop 
                 rospy.sleep(self.TASK_DELAY_TIME)   
         
@@ -32,28 +35,33 @@ class Gripper:
         rospy.wait_for_service('gripper_service')   
 
     def Conver_cmd_to_id(self, cmd):
-        if   (cmd == 'Catch_all'):   return 1
-        elif (cmd == 'Loosen_all'):  return 2
+        # print("aaaaaa\n")
+        if   (cmd == 'Catch_all'):      return 1
+        elif (cmd == 'Catch_all2'):     return 1 # delay 5 s
+        elif (cmd == 'Loosen_all'):     return 2
 
-        elif (cmd == 'Catch_no1'):   return 3
-        elif (cmd == 'Catch_no2'):   return 4
-        elif (cmd == 'Catch_no3'):   return 5
+        elif (cmd == 'Catch_no1'):      return 3
+        elif (cmd == 'Catch_no2'):      return 4
+        elif (cmd == 'Catch_no3'):      return 5
 
-        elif (cmd == 'Loosen_no1'):  return 6
-        elif (cmd == 'Loosen_no2'):  return 7
-        elif (cmd == 'Loosen_no3'):  return 8
+        elif (cmd == 'Loosen_no1'):     return 6
+        elif (cmd == 'Loosen_no2'):     return 7
+        elif (cmd == 'Loosen_no3'):     return 8
         
-        elif (cmd == 'Stop'):        return 0
-        elif (cmd == 'Disable'):     return 9
-        elif (cmd == 'rot_to_norm'):     return 10
-        elif (cmd == 'rot_to_parll'):     return 11
+        elif (cmd == 'Stop'):           return 0
+        elif (cmd == 'Disable'):        return 9
+        elif (cmd == 'rot_to_norm'):    return 10
+        elif (cmd == 'rot_to_parll'):   return 11
+        elif (cmd == 'aa'):             return 12
+        
         
 
 if __name__ == '__main__':
-    
     rospy.init_node('stm_client_', anonymous=True)
+    
     gripper = Gripper()
     gripper.Connect_to_Server()
+    
     gripper.Send_Gripper_Command('Loosen_all')
 
     print('Mission Complete!!!')
