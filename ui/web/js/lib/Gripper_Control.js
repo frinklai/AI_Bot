98308f9575fe9421e4082493290933452d95e863
frinklai <frinklai@gmail.com>
2019-06-01 21:43:31 +0800

    add some extra mode (catch_to_2D...) and cmd id

--- ui/web/js/lib/Gripper_Control.js
+++ ui/web/js/lib/Gripper_Control.js
@@ -49,15 +49,21 @@ function updata_feed_back_display(receieve_data_msg)
 		document.getElementById("DisplayTableimg").src = "img/system.png";
 
 	else if(receieve_data_msg==10)
-		document.getElementById("DisplayTableimg").src = "img/3D_mode.png";
-
-	else if(receieve_data_msg==11)
 		document.getElementById("DisplayTableimg").src = "img/drag_mode.png";
 
+	else if(receieve_data_msg==11) 
+		document.getElementById("DisplayTableimg").src = "img/2D_mode.png";
+
 	else if(receieve_data_msg==12)
-		document.getElementById("DisplayTableimg").src = "img/catch.png";
+		document.getElementById("DisplayTableimg").src = "img/3D_mode.png";
 	
 	else if(receieve_data_msg==13)
+		document.getElementById("DisplayTableimg").src = "img/stop.png";
+
+	else if(receieve_data_msg==14)
+		document.getElementById("DisplayTableimg").src = "img/catch.png";
+	
+	else if(receieve_data_msg==15)
 		document.getElementById("DisplayTableimg").src = "img/loosen.png";
 
 }
@@ -82,12 +88,15 @@ function send_gripper_cmd(id)
 		var is_success = res.success;
 	});
 
-	if((id!=0)&&(id!=5)&&(id!=13)) //stop
+	if((id!=0)&&(id!=5)&&(id!=15)) //stop
+	// if(id!=0) //if id is not 0, delay some time then stop
 	{
 		// wait 'GRIPPER_CMD_WAIT_TIME' ms, then run function -> $("#btn_stop_all").click(); 
-		if((id==10)||(id==11))
+		if((id==10)||(id==12))
 			setTimeout(function(){$("#btn_stop_all").click();}, BASE_ROTATION_WAIT_TIME);    // base rotation, wait 2s
 		else
+			if(id==5)
+				console.log('aaaaa');
 			setTimeout(function(){$("#btn_stop_all").click();}, CATCH_LOOSEN_WAIT_TIME);  	 // catch or loosen, wait 3s
 	}
 }
@@ -107,13 +116,13 @@ $("#btn_Loosen_all").click(function()
 
 $("#btn_2D_Catch").click(function()
 {
-	send_gripper_cmd(12) 
+	send_gripper_cmd(14) 
 	console.log('click btn_2D_Catch');
 });
 
 $("#btn_2D_Loosen").click(function()
 {
-	send_gripper_cmd(13)  											 					// loosen 2 finger
+	send_gripper_cmd(15)  											 					// loosen 2 finger
 	setTimeout(function(){$("#btn_stop_all").click();}, CATCH_LOOSEN_WAIT_TIME);  	 // catch or loosen, wait 3s
 	console.log('click btn_2D_Loosen');
 });
@@ -121,34 +130,52 @@ $("#btn_2D_Loosen").click(function()
 $("#btn_3D_catch_to_2D").click(function()
 {
 	send_gripper_cmd(5)  // when 											 					// Catch finger 3
-	setTimeout(function(){$(send_gripper_cmd(12)).click();}, CATCH_LOOSEN_WAIT_TIME);  // Catch 2 finger
+	setTimeout(function(){$(send_gripper_cmd(14)).click();}, CATCH_LOOSEN_WAIT_TIME);  // Catch 2 finger
 	console.log('click btn_2D_Catch');
 });
 
 $("#btn_2D_Loosen_to_3D").click(function()
 {
-	send_gripper_cmd(13)  											 					// loosen 2 finger
+	send_gripper_cmd(15)  											 					// loosen 2 finger
 	setTimeout(function(){$(send_gripper_cmd(8)).click();}, CATCH_LOOSEN_WAIT_TIME);   // loosen finger 3
 	console.log('click btn_2D_Loosen');
 });
 
+// --
+// $("#btn_3D_catch_to_2D").click(function()
+// {
+// 	send_gripper_cmd(11);  	
+// 	setTimeout(function(){$(send_gripper_cmd(5)).click();}, BASE_ROTATION_WAIT_TIME);
+// 	setTimeout(function(){$(send_gripper_cmd(14)).click();}, BASE_ROTATION_WAIT_TIME+CATCH_LOOSEN_WAIT_TIME+1000);
+// 	console.log('click btn_2D_Catch');
+// });
+
+
+// $("#btn_2D_Loosen_to_3D").click(function()
+// {
+// 	send_gripper_cmd(11)  // when 											 		   // rot to 2D_mode
+// 	setTimeout(function(){$(send_gripper_cmd(8)).click();}, BASE_ROTATION_WAIT_TIME);  // loosen finger 3
+// 	setTimeout(function(){$(send_gripper_cmd(15)).click();}, 6000);  // loosen 2 finger
+// 	console.log('click btn_2D_Loosen');
+// });
+
 // ================== rotate gripper base ==================
 
-$("#btn_rot2norm").click(function()
+$("#btn_rot2norm").click(function()			// 3D_mode
 {
-	send_gripper_cmd(10)
+	send_gripper_cmd(12)
 	console.log('click btn_rot2norm');
 });
 
-$("#btn_rot2parll").click(function()
+$("#btn_rot2parll").click(function() 		//drag_mode
 {
-	send_gripper_cmd(11)
+	send_gripper_cmd(10)
 	console.log('click btn_rot2parll');
 });
 
-$("#btn_rot2two_finger").click(function()
+$("#btn_rot2two_finger").click(function()	// 2D_mode
 {
-	// send_gripper_cmd(12)
+	send_gripper_cmd(11)
     console.log('click btn_rot2two_finger');
 });
 
@@ -199,7 +226,7 @@ $("#btn_stop_all").click(function()
 
 $("#btn_disable_all").click(function()
 {
-	send_gripper_cmd(9)
+	setTimeout(function(){$("#btn_stop_all").click();}, CATCH_LOOSEN_WAIT_TIME);  
     console.log('click btn_disable_all');
 });
 $("#btn_Connect").click(function()
