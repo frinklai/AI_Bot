c65c23a7c5a00b5657946d544c513a6633b620cd
frinklai <frinklai@gmail.com>
2019-07-22 16:01:45 +0800

    add new pkg - webcam_yolov3

---
+++ vision/webcam_yolov3/yolo_v3/scripts/path_set.py
@@ -0,0 +1,6 @@
+
+model_data_path         = "/home/iclab-arm/ros_yolo3/src/yolo3/"
+font_path               = "/home/iclab-arm/ros_yolo3/src/yolo3/"
+python3_site_pkg_path   = "/home/iclab-arm/.local/lib/python3.5/site-packages/"
+
+
