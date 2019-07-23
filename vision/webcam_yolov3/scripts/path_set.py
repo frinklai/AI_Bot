5e2e598f5945a121238b1cfe8ebb2ff80a308cfa
p <qaz12994@gmail.com>
2019-07-23 15:18:40 +0800

    add new webcam_yolov3 pkg

---
+++ vision/webcam_yolov3/scripts/path_set.py
@@ -0,0 +1,6 @@
+
+model_data_path         = "/home/iclab-arm/AI_Bot_ws/src/AI_Bot/vision/webcam_yolov3/"
+font_path               = "/home/iclab-arm/AI_Bot_ws/src/AI_Bot/vision/webcam_yolov3/"
+python3_site_pkg_path   = "/home/iclab-arm/.local/lib/python3.5/site-packages/"
+
+
