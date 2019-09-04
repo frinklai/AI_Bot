2e42e66c6e33fd5a5666b645f3fb3db429bfb681
p <qaz12994@gmail.com>
2019-09-04 16:48:27 +0800

    add speech_status

--- speech/src/speech_pub_add_SameObject.py
+++ speech/src/speech_pub_add_SameObject.py
@@ -8,9 +8,9 @@ import time
 # ros
 import rospy
 from speech.msg import SR
+from strategy.msg import speech_status
 name1 = " "
 name2 = " "
-status = 1
 
 def SpeechRecog():
     #Record Audio
@@ -32,23 +32,31 @@ def SpeechRecog():
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
+def get_speech_status(data):
+    speech.status = data.status
+
 if __name__ == '__main__':
     try:
         rospy.init_node('speech_pub', anonymous=True)
         speech_recognition_pub = rospy.Publisher("/speech/check", SR, queue_size=10)
         rate = rospy.Rate(1) # hz
         check = SR()
+        global speech
+        speech = speech_status()
+        rospy.Subscriber('/speech/status',speech_status , get_speech_status)
         while not rospy.is_shutdown():
             check.speech_check = 0
             check.confirm = 0
 
-            # if status == 0:
-            #     print("wait!!")
-            #     rospy.sleep(1)
-
-            if status == 1:
+            if speech.status == 0:
+                print("wait!!")
+                rospy.sleep(1)
+                # print(speech.status)
+            
+            elif speech.status == 1:
                 # name1 = SpeechRecog()
-                name1 = "去抓寶特瓶"
+                name1 = input('抓什麼\n')
+                # name1 = "去抓寶特瓶"
                 catch = name1.find(u'抓')
                 if catch != -1:
                     cnt = catch + 1
@@ -71,11 +79,11 @@ if __name__ == '__main__':
                     print('請說要"抓"什麼')
                 print('check = ' + str(check.speech_check))
                 if check.speech_check != 0:
-                    status = 2
+                    speech.status = 0
 
-            elif status == 2:
+            elif speech.status == 2:
                 # name2 = SpeechRecog()
-                name2 = input('是or不是')
+                name2 = input('是or不是\n')
                 for i in range(len(name2)):
                     if name2[i] == '是' or name2[i] == '對':
                         check.confirm = 1
@@ -85,14 +93,15 @@ if __name__ == '__main__':
                         break
                     else:
                         check.confirm = 0
-                print(check.confirm)
+                # print(check.confirm)
                 if check.confirm == 0:
                     print('不確定,再說一次')
                 elif check.confirm != 0:
-                    status = 1
+                    speech.status = 0
 
             for i in range(2):
                 speech_recognition_pub.publish(check)
                 rospy.sleep(1)
+
     except rospy.ROSInterruptException:
         pass
