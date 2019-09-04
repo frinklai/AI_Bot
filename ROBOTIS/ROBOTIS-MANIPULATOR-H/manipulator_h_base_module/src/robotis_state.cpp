1806e581f8739243826503e677c22a5fddbb320d
ChienMingYi <miketku151@gmail.com>
2019-09-04 13:47:14 +0800

    add new fuction

--- ROBOTIS/ROBOTIS-MANIPULATOR-H/manipulator_h_base_module/src/robotis_state.cpp
+++ ROBOTIS/ROBOTIS-MANIPULATOR-H/manipulator_h_base_module/src/robotis_state.cpp
@@ -59,14 +59,14 @@ RobotisState::RobotisState()
 /*************************************new_fuction****************************/
   IK_test  = new ManipulatorKinematicsDynamics(ARM);
 /****************************************************************************/  
-
+  
 }
 
 RobotisState::~RobotisState()
 {
 }
 /*****************************************new_fuction**********************************************/
-bool RobotisState::setInverseKinematics(int cnt, int all_steps, Eigen::MatrixXd start_rotation, double start_phi, Eigen::VectorXd Old_JointAngle)
+bool RobotisState::setInverseKinematics(int cnt, int all_steps, Eigen::MatrixXd start_rotation, double start_phi, Eigen::VectorXd Old_JointAngle, bool Line_phi)
 {
   for (int dim = 0; dim < 3; dim++)
     ik_target_position_.coeffRef(dim, 0) = calc_task_tra_.coeff(cnt, dim);
@@ -77,68 +77,75 @@ bool RobotisState::setInverseKinematics(int cnt, int all_steps, Eigen::MatrixXd 
                                        kinematics_pose_msg_.pose.orientation.x,
                                        kinematics_pose_msg_.pose.orientation.y,
                                        kinematics_pose_msg_.pose.orientation.z);
-  Eigen::Matrix3d goal_rotation = robotis_framework::convertQuaternionToRotation(target_quaternion);
-  if(cnt == 0)
+
+  if(Line_phi == false)
   {
-    is_inv = false;
-    int c;
-    bool ik_s = false;
-    bool limit_success;
-    //++
-    IK_test->do_wirst_avoid = false;
-    
-    Eigen::Vector3d test_position;
-    Eigen::MatrixXd test_rotation;
-    float test_phi;
-    float test_slide_pos;
-    for (int id = 0; id <= MAX_JOINT_ID; id++)
-      IK_test->manipulator_link_data_[id]->joint_angle_ = Old_JointAngle(id);
-       
-    for(float i=0; i<=12; i++)
+    Eigen::Matrix3d goal_rotation = robotis_framework::convertQuaternionToRotation(target_quaternion);
+    if(cnt == 0)
     {
-      c = int(((double)all_steps-1) * i/12);
-      for (int dim = 0; dim < 3; dim++)
-        test_position.coeffRef(dim, 0) = calc_task_tra_.coeff(c, dim);
-      Eigen::Quaterniond q = slerp(i/12, start_quaternion, target_quaternion, is_inv);
-      test_rotation = robotis_framework::convertQuaternionToRotation(q);
-      test_phi = start_phi + i/12 * (kinematics_pose_msg_.phi - start_phi);
-      test_slide_pos = calc_slide_tra_.coeff(c,0);
-      ik_s = IK_test->InverseKinematics_p2p(test_position, test_rotation, test_phi, test_slide_pos, Old_JointAngle, true);
+      is_inv = false;
+      int c;
+      bool ik_s = false;
+      bool limit_success;
+      //++
+      IK_test->do_wirst_avoid = false;
       
-      for (int id = 1; id <= 7; id++)
+      Eigen::Vector3d test_position;
+      Eigen::MatrixXd test_rotation;
+      float test_phi;
+      float test_slide_pos;
+      for (int id = 0; id <= MAX_JOINT_ID; id++)
+        IK_test->manipulator_link_data_[id]->joint_angle_ = Old_JointAngle(id);
+        
+      for(float i=0; i<=12; i++)
       {
-        if (IK_test->manipulator_link_data_[id]->joint_angle_ > IK_test->manipulator_link_data_[id]->joint_limit_max_)
+        c = int(((double)all_steps-1) * i/12);
+        for (int dim = 0; dim < 3; dim++)
+          test_position.coeffRef(dim, 0) = calc_task_tra_.coeff(c, dim);
+        Eigen::Quaterniond q = slerp(i/12, start_quaternion, target_quaternion, is_inv);
+        test_rotation = robotis_framework::convertQuaternionToRotation(q);
+        test_phi = start_phi + i/12 * (kinematics_pose_msg_.phi - start_phi);
+        test_slide_pos = calc_slide_tra_.coeff(c,0);
+        ik_s = IK_test->InverseKinematics_p2p(test_position, test_rotation, test_phi, test_slide_pos, Old_JointAngle, true, Line_phi);
+        
+        for (int id = 1; id <= 7; id++)
         {
-          limit_success = false;   
-          break;
+          if (IK_test->manipulator_link_data_[id]->joint_angle_ > IK_test->manipulator_link_data_[id]->joint_limit_max_)
+          {
+            limit_success = false;   
+            break;
+          }
+          else if (IK_test->manipulator_link_data_[id]->joint_angle_ < IK_test->manipulator_link_data_[id]->joint_limit_min_)
+          {
+            limit_success = false;   
+            break;
+          }
+          else
+            limit_success = true;
         }
-        else if (IK_test->manipulator_link_data_[id]->joint_angle_ < IK_test->manipulator_link_data_[id]->joint_limit_min_)
-        {
-          limit_success = false;   
-          break;
+        if(!ik_s || !limit_success){
+  
+          if(!is_inv){
+            is_inv = true;
+            i = -1;
+          }else
+            return false;
         }
-        else
-          limit_success = true;
-      }
-      if(!ik_s || !limit_success){
-        if(!is_inv){
-          is_inv = true;
-          i = -1;
-        }else
-          return false;
       }
     }
+    double count = (double) cnt / (double) all_steps;
+    ik_target_quaternion = slerp(count, start_quaternion, target_quaternion, is_inv);
+    inv_target_quaternion = slerp(count, start_quaternion, target_quaternion, true);
+
+    ik_target_phi_ = start_phi + count * (kinematics_pose_msg_.phi - start_phi);
+    ik_target_rotation_ = robotis_framework::convertQuaternionToRotation(ik_target_quaternion);
   }
-
-
-  double count = (double) cnt / (double) all_steps;
-
-  ik_target_quaternion = slerp(count, start_quaternion, target_quaternion, is_inv);
-  inv_target_quaternion = slerp(count, start_quaternion, target_quaternion, true);
-
-  ik_target_phi_ = start_phi + count * (kinematics_pose_msg_.phi - start_phi);
-
-  ik_target_rotation_ = robotis_framework::convertQuaternionToRotation(ik_target_quaternion);
+  else  
+  {
+    double count = (double) cnt / (double) all_time_steps_;
+    Eigen::Quaterniond quaternion = start_quaternion.slerp(count, target_quaternion);
+    ik_target_rotation_ = robotis_framework::convertQuaternionToRotation(quaternion);
+  }                                 
   return true;
 }
 
