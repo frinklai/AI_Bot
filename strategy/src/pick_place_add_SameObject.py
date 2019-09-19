7614699b5bf87e95329b2ed392099fdc4e50303a
p <qaz12994@gmail.com>
2019-09-19 19:00:49 +0800

    fix camera wrong

--- strategy/src/pick_place_add_SameObject.py
+++ strategy/src/pick_place_add_SameObject.py
@@ -21,7 +21,7 @@ from strategy.msg import speech_status
 count   = 0
 
 PICKORDER = 0
-SPEED_L     = 30
+SPEED_L     = 10
 
 idle                    = 0
 busy                    = 1
@@ -77,7 +77,7 @@ class stockingTask:
         if self.name == 'left':
             self.is_right = -1
             self.speed = SPEED_L
-            self.faster_speed = 40
+            self.faster_speed = 10
         self.init_pub_sub()
         if self.en_sim:
             self.suction = SuctionTask(self.name + '_gazebo')
@@ -107,12 +107,12 @@ class stockingTask:
                 self.nowState = self.nextState
                 return  
 
-        #(第一個動作)
+        #(第一個動作 safepoint)
         elif self.state == initPose:
             print('self.state == initPose')
             self.state = busy
             self.arm.set_speed(self.speed)
-            self.pos   = [0.4, 0.5, -0.3]
+            self.pos   = [0, 0.5, -0.5]
             self.euler = [0, 0, 0]
             self.phi = 0
             self.nextState = wait_speech_recognition
@@ -128,9 +128,9 @@ class stockingTask:
                 if self.check.speech_check == 1:
                     self.speech_obj_name = 'bottle'
                 elif self.check.speech_check == 2:
-                    self.speech_obj_name = 'cellphone'
+                    self.speech_obj_name = 'paper'
                 elif self.check.speech_check == 3:
-                    self.speech_obj_name = 'mouse'
+                    self.speech_obj_name = 'metal'
                 self.nextState = wait_img_pos
                 self.No_Object_count = 0
                 self.check.confirm = 0
@@ -147,42 +147,51 @@ class stockingTask:
             self.state = busy
             self.cnt = 0
             for i in range(len(self.img_data_list)):
-                # if (self.img_data.min_x > 638) and (self.img_data.min_y > 156) and (self.img_data.Max_x < 1188) and (self.img_data.Max_y < 811):
-                if self.speech_obj_name == self.img_data.object_name:
-                    print("----- stra detect object_" + str(i) + " ----- ")
-                    print("object_name = " + str(self.img_data_list[i].object_name))
-                    print("score = " + str(self.img_data_list[i].score))
-                    print("min_xy = [ " +  str( [self.img_data_list[i].min_x, self.img_data_list[i].min_y] ) +  " ]" )
-                    print("max_xy = [ " +  str( [self.img_data_list[i].Max_x, self.img_data_list[i].Max_y] ) +  " ]" )
-                    self.obj_name = self.img_data.object_name
-                    # self.nextState = move_to_obj
-                    global x,y
-                    x = (self.img_data_list[i].min_x + self.img_data_list[i].Max_x)/2
-                    y = (self.img_data_list[i].min_y + self.img_data_list[i].Max_y)/2
-                    self.move_to_obj(x, y)
-                    time.sleep(1)
+                if (self.img_data.min_x > 638) and (self.img_data.min_y > 156) and (self.img_data.Max_x < 1188) and (self.img_data.Max_y < 811):
+                    if self.speech_obj_name == self.img_data.object_name:
+                        print("----- stra detect object_" + str(i) + " ----- ")
+                        print("object_name = " + str(self.img_data_list[i].object_name))
+                        print("score = " + str(self.img_data_list[i].score))
+                        print("min_xy = " +  str( [self.img_data_list[i].min_x, self.img_data_list[i].min_y] ) )
+                        print("max_xy = " +  str( [self.img_data_list[i].Max_x, self.img_data_list[i].Max_y] ) )
+                        self.obj_name = self.img_data.object_name
+                        # self.nextState = move_to_obj
+                        global x,y
+                        x = (self.img_data_list[i].min_x + self.img_data_list[i].Max_x)/2
+                        y = (self.img_data_list[i].min_y + self.img_data_list[i].Max_y)/2
+                        self.move_to_obj(x, y)
+                        time.sleep(1)
 
-                    self.speech.status = 2
+                        self.speech.status = 2
 
-                    while self.check.confirm == 0:
-                        speech_status_pub.publish(self.speech)
-                        rospy.sleep(.1)
-                    if self.check.confirm == 1:
-                        print('get this')
-                        self.nextState = down
-                        break
-                    elif self.check.confirm == 2:
-                        print('not this')
-                        self.check.confirm = 0
-                        if i == (len(self.img_data_list)) - 1:
-                            self.nextState = initPose
+                        while self.check.confirm == 0:
+                            speech_status_pub.publish(self.speech)
+                            rospy.sleep(.1)
+                        if self.check.confirm == 1:
+                            print('get this')
+                            self.nextState = down
                             break
+                        elif self.check.confirm == 2:
+                            print('not this')
+                            self.check.confirm = 0
+                            #防止攝影機出錯
+                            self.arm.set_speed(self.speed)
+                            self.pos   = [0, 0.5, -0.5]
+                            self.euler = [0, 0, 0]
+                            self.phi = 0
+                            self.arm.ikMove(mode= 'p2p', pos = self.pos, euler = self.euler, phi = self.phi)
+                            rospy.sleep(8)
+                            if i == (len(self.img_data_list)) - 1:
+                                self.nextState = initPose
+                                break
+                    else:
+                        print('not this object')
+                        self.No_Object_count += 1
+                        break
                 else:
-                    print('not this object')
+                    print('object over range!!')
                     self.No_Object_count += 1
                     break
-                # else:
-                #     print('object over range!!')
             if(len(self.img_data_list) == 0):
                 print('no object!!!')
                 self.nextState = wait_img_pos
@@ -195,14 +204,16 @@ class stockingTask:
                 rospy.sleep(2)
                 # self.close_box = True                          
 
-        # # 防止影像狀態機怪怪的空狀態(必定接在wait_img_pos後面)
+        # 防止影像狀態機怪怪的空狀態(必定接在wait_img_pos後面)
         # elif self.state == goto:              
         #     print('self.state == goto')
         #     self.state = busy
-        #     if self.standby_safeFlag == True:
-        #         self.nextState = standby_safe_point
-        #     else:
-        #         self.nextState = move_standby
+        #     self.arm.set_speed(self.speed)
+        #     self.pos   = [0, 0.5, -0.5]
+        #     self.euler = [0, 0, 0]
+        #     self.phi = 0
+        #     self.nextState = wait_speech_recognition
+        #     self.arm.ikMove(mode= 'p2p', pos = self.pos, euler = self.euler, phi = self.phi) 
         
 
         # elif self.state == move_to_obj:          
@@ -225,7 +236,7 @@ class stockingTask:
             self.arm.set_speed(self.faster_speed)
             self.state = busy
             self.nextState = down_sec
-            self.pos   = [0, 0, -0.1]
+            self.pos   = [0, 0, -0.048]
             self.arm.relative_move_pose(mode='p2p', pos=self.pos)
             rospy.sleep(.1)
 
@@ -234,7 +245,7 @@ class stockingTask:
             self.arm.set_speed(self.speed)
             self.state = busy
             self.nextState = pickObject  
-            self.pos  = [0, 0, -0.1]
+            self.pos  = [0, 0, -0.048]
             self.arm.relative_move_pose(mode='p2p', pos=self.pos)
 
         elif self.state == pickObject:
@@ -255,15 +266,16 @@ class stockingTask:
             self.arm.set_speed(self.faster_speed)
             self.state = busy
             self.nextState = moveObject
-            self.pos   = [0, 0, 0.2]
+            self.pos   = [0, 0, 0.196]
             self.arm.relative_move_pose(mode='p2p', pos=self.pos)
+
 
         elif self.state == moveObject:
             print('self.state == moveObject')
             self.arm.set_speed(self.faster_speed)
             self.state = busy
             self.nextState = placeObject
-            self.pos   = [0.4, 0.5, -0.3]
+            self.pos   = [0.1, 0.5, -0.4]
             self.euler = [0, 0, 0]
             self.phi = 0
             self.arm.ikMove(mode= 'p2p', pos = self.pos, euler = self.euler, phi = self.phi) 
@@ -277,13 +289,13 @@ class stockingTask:
 
     #-------------------------移動物件------------------------------------------------------
     def move_to_obj(self, x, y):
-        print("move to xy = [ " + str([x,y]) + " ]")
+        print("move to xy = " + str([x,y]) )
         self.arm.set_speed(self.faster_speed)
         posX , posY = self.Image_transform(x, y)
         self.state = busy
         self.nextState = wait_img_pos
-        self.pos   = [round(posX, 4), round(posY, 4), -0.45]
-        self.pos   = [0.3, 0.4, -0.45]
+        self.pos   = [round(posX, 4), round(posY, 4), -0.5]
+        # self.pos   = [0.3, 0.4, -0.5]
         print(self.pos)
         self.euler = [0, 0, 0]
         self.phi = 0
@@ -291,8 +303,9 @@ class stockingTask:
 
     #-------------------------座標轉換------------------------------------------------------
     def Image_transform(self, Camera_Image_X, Camera_Image_Y):
-        Arm_posX = (866 - Camera_Image_Y)*0.000889 - 0.4795     
-        Arm_posY = (974 - Camera_Image_X)*0.000889 + 0.3960     
+        Arm_posX = (866 - Camera_Image_Y)*0.000889 - 0.4795 - 0.0700
+        # Arm_posY = (974 - Camera_Image_X)*0.000859 + 0.3960 
+        Arm_posY = (974 - Camera_Image_X)*0.000662 + 0.388332
         return Arm_posX, Arm_posY
 
 
